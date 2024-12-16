1d15a37c6f58f5e46b5d9ca63d4d7518d1a80fa8
Kacper Moll <kacper.moll@lingarogroup.com>
2024-12-16 23:23:14 +0100

    add streamlit initial app

---
+++ frontend/streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+
+def login():
+    if st.button("Log in"):
+        st.session_state.logged_in = True
+        st.rerun()
+
+
+def logout():
+    if st.button("Log out"):
+        st.session_state.logged_in = False
+        st.rerun()
+
+login_page = st.Page(login, title="Log in", icon=":material/login:")
+logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
+
+
+if st.session_state.logged_in:
+    pg = st.navigation(
+        {
+            "Account": [logout_page],
+        }
+    )
+else:
+    pg = st.navigation([login_page])
+
+pg.run()
